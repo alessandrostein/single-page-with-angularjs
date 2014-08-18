ab1b9397350045bd57ee1b164a10b251a93ee264
alessandrostein <alessandro@alessandrostein.com>
2014-08-18 11:17:30 -0300

    Create controller.js

---
+++ app/controllers/controllers.js
@@ -0,0 +1,11 @@
+app.controller('HomeCtrl', function($rootScope, $location){
+	$rootScope.activetab = $location.path();
+});
+
+app.controller('SobreCtrl', function($rootScope, $location){
+	$rootScope.activetab = $location.path();
+});
+
+app.controller('ContatoCtrl', function($rootScope, $location){
+	$rootScope.activetab = $location.path();
+});
