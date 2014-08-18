468372441926de5763b507d22d44a04455d43c4d
Fabricio Ronchi <fabricioronchii@gmail.com>
2014-08-18 14:12:57 -0300

    Teste

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,12 +1,12 @@
-angular.module('appSinglePage').controller('HomeCtrl', function($scope){
-	$scope.activetab = "/";
+angular.module('appSinglePage').controller('HomeCtrl', function($scope, $rootScope){
+	$rootScope.activetab = "/";
 });
 
-angular.module('appSinglePage').controller('SobreCtrl', function($scope){
-	$scope.activetab = "/sobre";
+angular.module('appSinglePage').controller('SobreCtrl', function($scope, $rootScope){
+	$rootScope.activetab = "/sobre";
 });
 
-angular.module('appSinglePage').controller('ContatoCtrl', function($scope){
-	$scope.activetab = "/contato";
+angular.module('appSinglePage').controller('ContatoCtrl', function($scope, $rootScope){
+	$rootScope.activetab = "/contato";
 });
 
